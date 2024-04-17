664fd7c2624d7059b92f41aa35e6d2604b8b1742
ldur <lassedur@gmail.com>
2024-04-17 08:17:47 +0200

    hello github

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -7,7 +7,7 @@ import io.ktor.server.routing.*
 fun Application.configureRouting() {
     routing {
         get("/") {
-            call.respondText("Hello Lasse!")
+            call.respondText("Hello Lasse! til Githyub")
         }
     }
 }
